a1e40b66a519e737d0abf650e1c005a088cea521
Yasim <91527193+Yasimcd@users.noreply.github.com>
2022-12-16 14:59:33 -0800

    Update brownies.cpp

--- brownies.cpp
+++ brownies.cpp
@@ -1,12 +1,11 @@
 // brownies.cpp 
 // This program determines how many brownies will fit in a baking pan of a specified size. 
 // Yasim Cruz 
-#include <iostream> // INCLUDE THE FILE NEEDED TO DO I/O
+#include <iostream> 
 using namespace std;
 
 int main()
-{   // DECLARE ALL NEEDED VARIABLES HERE. GIVE EACH ONE A DESCRIPTIVE
-   // NAME AND AN APPROPRIATE DATA TYPE.
+{   //NEEDED VARIABLES 
 
    int length = 0,         
           width = 0,          
@@ -16,23 +15,21 @@ int main()
           largePortion,
           smallPortion;         
    
-   // WRITE STATEMENTS HERE TO PROMPT FOR AND INPUT THE INFORMATION
-   // THE PROGRAM NEEDS TO GET FROM THE USER.
+   // PROMPT FOR AND INPUT THE INFORMATION THE PROGRAM NEEDS TO GET FROM THE USER.
    cout << "Enter baking pan length (in inches): ";
    cin  >> length;
    
    cout << "Enter baking pan width (in inches): ";
    cin  >> width; 
    
-   // WRITE STATEMENTS HERE TO PERFORM ALL NEEDED COMPUTATIONS  
-   // AND ASSIGN THE RESULTS TO VARIABLES.
+   // PERFORM ALL NEEDED COMPUTATIONS AND ASSIGN THE RESULTS TO VARIABLES.
    largeBrownies = 2 * 2;
    smallBrownies = 1 * 1;
    panArea = length * width; 
    largePortion = panArea/largeBrownies;
    smallPortion = panArea/smallBrownies; 
    
-   // WRITE STATEMENTS HERE TO DISPLAY THE REQUESTED INFORMATION.
+   //DISPLAY THE REQUESTED INFORMATION.
    cout << "A " << length << " x " << width << " inch pan can hold ";
    cout << smallPortion << " small brownies " << "or ";
    cout << largePortion << " large brownies." << endl;
